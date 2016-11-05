1d448e1fb1b5042a63f5787b351ad207ce5cbbeb
Fabio Suenaga <fabiosuenaga@topinformation.com.br>
2016-11-05 21:28:13 -0200

    - Created json of person to test the request. - List the persons using ng-repeat. - Created the success response of get(‘/contato’).

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,5 +1,15 @@
 var myApp = angular.module('myApp',[]);
-myApp.controller('AppCtrl',['$scope','$http',function AppCtrl() {
-												console.log("Hello World from controller")
-											}
+myApp.controller('AppCtrl',['$scope','$http',
+
+	function AppCtrl($scope, $http) {
+		
+		console.log("Hello World from controller");
+
+		$http.get('/contato').success(function(response) {
+			console.log('I got the data I requested')
+			$scope.contactList = response;
+		})
+
+	}
+
 ]);
--- server.js
+++ server.js
@@ -3,5 +3,36 @@ var app 	= express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contato',function(req, res){
+	console.log('Get Request received!')
+
+	person1 = {
+		name: 'Nome 1',
+		email: 'nome1@email.com',
+		telefone: '(41) 9999-9999'
+	};									
+		
+	person2 = {
+		name: 'Nome 2',
+		email: 'nome2@email.com',
+		telefone: '(42) 9999-9999'
+	};
+
+	person3 = {
+		name: 'Nome 3',
+		email: 'nome3@email.com',
+		telefone: '(43) 9999-9999'
+	};
+
+	person4 = {
+		name: 'Nome 4',
+		email: 'nome4@email.com',
+		telefone: '(44) 9999-9999'
+	};
+
+	var contactList = [person1, person2, person3, person4];
+	res.json(contactList);
+});
+
 app.listen(3000);
 console.log("Server running on port 3000");
