a74c64d632a35447568bbe9e364b91d3b748ce93
Fabio Suenaga <fabiosuenaga@topinformation.com.br>
2016-11-05 22:54:30 -0200

    - Added mongojs dependency. - Removed dummy data from the response of get (/contato). - Added the data from the mongo to the response of get (/contato).

--- server.js
+++ server.js
@@ -1,37 +1,43 @@
 var express = require('express')
 var app 	= express();
+var mongojs = require('mongojs')
+var db		= mongojs('curitibaVegana',['contactList'])
 
 app.use(express.static(__dirname + "/public"));
 
 app.get('/contato',function(req, res){
 	console.log('Get Request received!')
 
-	person1 = {
-		name: 'Nome 1',
-		email: 'nome1@email.com',
-		telefone: '(41) 9999-9999'
-	};									
+	db.curitibaVegana.find(function(err, docs){
+		console.log(docs);
+		res.json(docs);
+	});
+	// person1 = {
+	// 	name: 'Nome 1',
+	// 	email: 'nome1@email.com',
+	// 	telefone: '(41) 9999-9999'
+	// };									
 		
-	person2 = {
-		name: 'Nome 2',
-		email: 'nome2@email.com',
-		telefone: '(42) 9999-9999'
-	};
+	// person2 = {
+	// 	name: 'Nome 2',
+	// 	email: 'nome2@email.com',
+	// 	telefone: '(42) 9999-9999'
+	// };
 
-	person3 = {
-		name: 'Nome 3',
-		email: 'nome3@email.com',
-		telefone: '(43) 9999-9999'
-	};
+	// person3 = {
+	// 	name: 'Nome 3',
+	// 	email: 'nome3@email.com',
+	// 	telefone: '(43) 9999-9999'
+	// };
 
-	person4 = {
-		name: 'Nome 4',
-		email: 'nome4@email.com',
-		telefone: '(44) 9999-9999'
-	};
+	// person4 = {
+	// 	name: 'Nome 4',
+	// 	email: 'nome4@email.com',
+	// 	telefone: '(44) 9999-9999'
+	// };
 
-	var contactList = [person1, person2, person3, person4];
-	res.json(contactList);
+	// var contactList = [person1, person2, person3, person4];
+	// res.json(contactList);
 });
 
 app.listen(3000);
