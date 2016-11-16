c072c926b8f05a410edf9fc116d27797bada0974
Fabio Suenaga <fabiosuenaga@topinformation.com.br>
2016-11-15 22:16:20 -0200

    - Layout improvements. - Added Ui Router (stateProviders and urlRouterProviders).

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -1,72 +1,97 @@
-var myApp   = angular.module('myApp',[]);
+var myApp = angular.module('myApp', ['ui.router']);
+
+myApp.config(function($stateProvider, $urlRouterProvider) {
+    
+    $urlRouterProvider.otherwise('/contato');
+    
+    $stateProvider
+        
+        // Contato
+        .state('contato', {
+            url: '/contato',
+            templateUrl: '/veg/contato/contato.html'
+        })
+        
+        // Noticias
+        .state('noticias', {
+            url: '/noticias',
+            template: '/veg/noticias/noticias.html'
+        })
+        
+        // Receitas
+        .state('receitas', {
+            url: '/receitas',
+            template: '/veg/receitas/receitas.html'           
+        })
+        
+});
 
 myApp.controller('AppCtrl',['$scope','$http',
 
-	function AppCtrl($scope, $http, uiGmapGoogleMapApi) {
-		
-		var refresh = function() {
-			$http.get('/contato').success(function(response) {
-				console.log('I got the data I requested')
-				$scope.contactList = response;
-				$scope.contact = "";
-			});
-		}
+    function AppCtrl($scope, $http, uiGmapGoogleMapApi) {
+        
+        var refresh = function() {
+            $http.get('/contato').success(function(response) {
+                console.log('I got the data I requested')
+                $scope.contactList = response;
+                $scope.contact = "";
+            });
+        }
 
-		refresh();
+        refresh();
 
-		// CRUD functions
-		$scope.addContact = function() {
-			console.log($scope.contact)
-			$http.post('/contato', $scope.contact).success(function(response) {
-				console.log(response)
-				refresh();
-			});
-		}; 
+        // CRUD functions
+        $scope.addContact = function() {
+            console.log($scope.contact)
+            $http.post('/contato', $scope.contact).success(function(response) {
+                console.log(response)
+                refresh();
+            });
+        }; 
 
-		$scope.remove = function(id) {
-			console.log(id);
-			$scope.idSelectedContact = null;
-			$scope.deselect = false;
-			$http.delete('/contato/' + id).success(function(response) {
-				refresh();
-			});
-		};
+        $scope.remove = function(id) {
+            console.log(id);
+            $scope.idSelectedContact = null;
+            $scope.deselect = false;
+            $http.delete('/contato/' + id).success(function(response) {
+                refresh();
+            });
+        };
 
-		$scope.edit = function(id) {
-			console.log(id);
-			$http.get('/contato/' + id).success(function(response) {
-				$scope.contact = response;
-			});
-		};
+        $scope.edit = function(id) {
+            console.log(id);
+            $http.get('/contato/' + id).success(function(response) {
+                $scope.contact = response;
+            });
+        };
 
-		$scope.update = function() {
-			console.log($scope.contact._id);
-			$scope.idSelectedContact = null;
-			$scope.deselect = false;
-			$http.put('/contato/' + $scope.contact._id, $scope.contact).success(function(response){
-				refresh();
-			})
-		};
+        $scope.update = function() {
+            console.log($scope.contact._id);
+            $scope.idSelectedContact = null;
+            $scope.deselect = false;
+            $http.put('/contato/' + $scope.contact._id, $scope.contact).success(function(response){
+                refresh();
+            })
+        };
 
-		$scope.deselect = function() {
-			$scope.contact = "";
-			$scope.idSelectedContact = null;
-			$scope.deselect = false;
-		};
+        $scope.deselect = function() {
+            $scope.contact = "";
+            $scope.idSelectedContact = null;
+            $scope.deselect = false;
+        };
 
-		$scope.idSelectedContact = null;
-		$scope.deselect = null;
-    	$scope.setSelected = function(idSelectedContact) {
-       	$scope.idSelectedContact = idSelectedContact;
+        $scope.idSelectedContact = null;
+        $scope.deselect = null;
+        $scope.setSelected = function(idSelectedContact) {
+        $scope.idSelectedContact = idSelectedContact;
 
-		if(idSelectedContact){
-       		$scope.deselect = true;		
-		}
+        if(idSelectedContact){
+            $scope.deselect = true;     
+        }
 
 
     }
 
-	}
+    }
 
 ]);
-
